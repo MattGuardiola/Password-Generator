1eb012926b124d02f4244b6b12893a86de9d48d7
Matt Guardiola <matt.g.guardiola@gmail.com>
2023-07-07 23:48:59 -0500

    now tasting for spaces within input fields

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const paswordLengthAlert = document.getElementById("password-length-alert")
     const uppercaseLetterAlert = document.getElementById("password-uppercase-alert")
     const specialCharacterAlert = document.getElementById("password-special-character-alert")
+    const spaceAlert = document.getElementById("password-space-alert")
 
 
 
@@ -47,13 +48,23 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
+    function handleSpaces() {
+        const password = passwordBox.value;
+        let doesContainSpaces = /[" "]/.test(password)
+
+        if (doesContainSpaces !== true) {
+            spaceAlert.classList.remove("alert-danger");
+            spaceAlert.classList.add("alert-success");
+        } else {
+            spaceAlert.classList.remove("alert-success");
+            spaceAlert.classList.add("alert-danger")
+        }
+    }
 
 
 
 
-
-
-
+    passwordBox.addEventListener("input", handleSpaces)
     passwordBox.addEventListener("input", handlePasswordLength)
     passwordBox.addEventListener("input", handleUppercase)
     passwordBox.addEventListener("input", handleSpecialCharacters)
